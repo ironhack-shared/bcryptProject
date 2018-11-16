3918f2bf4fad34f3cb4d94ea6af3b7984af3a35f
Ricardo Cefalo <ricardo.cefalo@gmail.com>
2018-11-16 17:49:07 +0100

    Register and log in

--- app.js
+++ app.js
@@ -1,116 +1,142 @@
-const port = 3000
-const session = require("express-session");
-const express = require('express')
+const port = 3000;
+const session = require('express-session');
+const express = require('express');
+
 const router = express.Router();
-const app = express()
-const MongoStore = require("connect-mongo")(session);
-const mongoose = require("mongoose")
-const bodyParser = require('body-parser')
-const bcrypt = require("bcrypt");
-const path = require("path")
-const User = require("./models/user")
-const genericUser = new User()
+const app = express();
+const MongoStore = require('connect-mongo')(session);
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt');
+const path = require('path');
+const User = require('./models/User');
+
+const genericUser = new User();
 
 mongoose
-	.connect('mongodb://localhost/bcrypt', {
-		useNewUrlParser: true
-	})
-	.then(x => {
-		console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
-	})
-	.catch(err => {
-		console.error('Error connecting to mongo', err)
-	});
+  .connect('mongodb://localhost/bcrypt', {
+    useNewUrlParser: true,
+  })
+  .then((x) => {
+    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`);
+  })
+  .catch((err) => {
+    console.error('Error connecting to mongo', err);
+  });
 
 app.use(bodyParser.urlencoded({
-	extended: true
+  extended: true,
 }));
 app.use(bodyParser.json());
 app.use(session({
-	secret: "basic-auth-secret",
-	cookie: {
-		maxAge: 60000
-	},
-	store: new MongoStore({
-		mongooseConnection: mongoose.connection,
-		ttl: 24 * 60 * 60 // 1 day
-	})
+  secret: 'basic-auth-secret',
+  cookie: {
+    maxAge: 60000,
+  },
+  store: new MongoStore({
+    mongooseConnection: mongoose.connection,
+    ttl: 24 * 60 * 60, // 1 day
+  }),
 }));
 router.use((req, res, next) => {
-	if (req.session.currentUser) {
-		next();
-	} else {
-		res.redirect("/login");
-	}
+  if (req.session.currentUser) {
+    next();
+  } else {
+    res.redirect('/login');
+  }
 });
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
-app.get('/secret', function (req, res) {
-	if (req.session.inSession) {
-		let sessionData = { ...req.session
-		}
-		res.render('secret', {
-			sessionData
-		})
-	} else {
-		res.render('404')
-	}
-})
+app.get('/secret', (req, res) => {
+  if (req.session.inSession) {
+    const sessionData = { ...req.session  };
+    res.render('secret', {
+      sessionData,
+    });
+  } else {
+    res.render('404');
+  }
+});
 
-app.get('/login', function (req, res) {
-	res.render('login')
-})
+app.get('/signup', (req, res) => {
+  res.render('signup');
+});
 
-app.post('/login', function (req, res) {
-	User.findOne({
-		user: req.body.user
-	}).then(found => {
-		const matches = bcrypt.compareSync(req.body.password, found.password)
+app.post('/signup', (req, res) => {
+  const { user, password } = req.body;
+  const saltRounds = 5;
+  const salt = bcrypt.genSaltSync(saltRounds);
+  const hash = bcrypt.hashSync(password, salt);
+  const newUser = new User({
+    user,
+    password : hash,
+  });
 
-		if (matches) {
-			req.session.inSession = true
-			req.session.user = req.body.user
+  newUser.save().then((user) => {
+    console.log(user);
+    res.redirect('/login');
+  });
+});
 
-			res.redirect('secret')
-		} else {
-			req.session.inSession = false
-			res.redirect('login')
-		}
-	})
-})
+app.get('/login', (req, res) => {
+  res.render('login');
+});
 
-app.get('/loggedOut', function (req, res) {
-	res.render('loggedOut')
-})
+app.post('/login', (req, res) => {
+  User.findOne({
+    user: req.body.user,
+  }).then((user) => {
+    if (!user) {
+      res.redirect('/login');
+      return;
+    }
+    console.log(user);
+    const matches = bcrypt.compareSync(req.body.password, user.password);
 
-app.get('/logout', function (req, res) {
-	req.session.destroy(() => {
-		req.session = null;
-		res.redirect('loggedOut')
-	})
-})
+    if (matches) {
+      req.session.inSession = true;
+      req.session.user = req.body.user;
 
-app.post('/createUser', function (req, res) {
-	const saltRounds = 5;
+      res.redirect('secret');
+    } else {
+      req.session.inSession = false;
+      res.redirect('login');
+    }
+  });
+});
 
-	genericUser.user = req.body.user
+app.get('/loggedOut', (req, res) => {
+  res.render('loggedOut');
+});
 
-	const salt = bcrypt.genSaltSync(saltRounds);
-	const hash = bcrypt.hashSync(req.body.password, salt);
+app.get('/logout', (req, res) => {
+  req.session.destroy(() => {
+    req.session = null;
+    res.redirect('loggedOut');
+  });
+});
 
-	genericUser.password = hashedPassword
+app.post('/createUser', (req, res) => {
+  const saltRounds = 5;
 
-	genericUser.save().then(x => {
-		req.session.inSession = true
+  genericUser.user = req.body.user;
 
-		res.json({
-			inSessionCreated: true
-		})
-	})
-})
+  const salt = bcrypt.genSaltSync(saltRounds);
+  const hash = bcrypt.hashSync('123', salt);
 
-app.listen(port, function () {
-	console.log(`Example app listening on port ${port}!`)
-})
+  genericUser.password = hashedPassword;
+
+  genericUser.save().then((x) => {
+    req.session.inSession = true;
+
+    res.json({
+      inSessionCreated: true,
+    });
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}!`);
+});
